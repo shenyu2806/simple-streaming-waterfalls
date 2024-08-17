2751406017c2cff3143455f11b77f5b3c59f7771
神月恭平 <tuzwyx@foxmail.com>
2024-08-17 19:10:17 +0800

    24/8/17 v1.0.0

---
+++ serve/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+// 导入 cors 中间件——支持跨域访问
+const cors = require('cors')
+
+ 
+const app = express();
+const port = 3000;
+app.use(cors()) 
+
+//静态托管
+const multer = require("multer")
+const upload = multer({dest:'./public/upload'})
+app.use(upload.any())
+// 设置静态文件目录
+app.use(express.static("./public"))
+
+//路由
+const indexRouter =require('./router/index')
+app.use('/',indexRouter)
+
+ 
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
+
+//打印错误，防止异常
+process.on('uncaughtException', function (err) { 
+    //打印出错误 
+    console.log(err); 
+    //打印出错误的调用栈方便调试 
+    console.log(err.stack);
+});
